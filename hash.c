af3304146268556f22473d215eb88749743fe81e
vinicius023 <viniciusbatista023@gmail.com>
2016-07-31 17:47:16 -0300

    hash.c parcial atualizado

--- hash.c
+++ hash.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "hash.h"
 
 #define TAM 1023
 
@@ -9,28 +10,29 @@ typedef union noh {
 	int pLexema;
 } noh;
 
-typedef struct Yylval {
+struct s_Yylval {
 	int flag;
 	noh lexema;
 	int linha;
 	int coluna;
-	//Yylval *prox;
-} Yylval;
+	Yylval *prox;
+};
 
 typedef struct hashTable {
 	int key;
 	Yylval yylval;
 } hashTable;
 
+
 void criaTabela(hashTable *tabela) {
 	tabela = malloc(TAM*sizeof(hashTable));
 	for (int i = 0; i < TAM; ++i) {
 		tabela[i].key = -1;
-		tabela[i].yylval.flag = -1;
-		tabela[i].yylval.lexema.pLexema = -1;
-		tabela[i].yylval.linha = -1;
-		tabela[i].yylval.coluna = -1;
-		//tabela[i].yylval.prox = NULL;
+		tabela[i].yylval->flag = -1;
+		tabela[i].yylval->lexema.pLexema = -1;
+		tabela[i].yylval->linha = -1;
+		tabela[i].yylval->coluna = -1;
+		tabela[i].yylval->prox = NULL;
 	}
 }
 
@@ -42,10 +44,10 @@ void addTabela(hashTable *tabela, Yylval token, int chave) {
 void imprimirTabela(hashTable *tabela) {
 	int chave = 0;
 	while(tabela[chave].key != -1) {
-		if (tabela[chave].yylval.flag == 0) {
-			printf("lexema: %s\t linha: %d\t coluna: %d\n", tabela[chave].yylval.lexema.lexema, tabela[chave].yylval.linha, tabela[chave].yylval.coluna);
+		if (tabela[chave].yylval->flag == 0) {
+			printf("lexema: %s\t linha: %d\t coluna: %d\n", tabela[chave].yylval->lexema.lexema, tabela[chave].yylval->linha, tabela[chave].yylval->coluna);
 		} else {
-			printf("lexema: %d\t linha: %d\t coluna: %d\n", tabela[chave].yylval.lexema.pLexema, tabela[chave].yylval.linha, tabela[chave].yylval.coluna);
+			printf("lexema: %d\t linha: %d\t coluna: %d\n", tabela[chave].yylval->lexema.pLexema, tabela[chave].yylval->linha, tabela[chave].yylval->coluna);
 		}
 		chave++;
 	}
